e2d95bb85c5a893ccf66d4140f8de895de30ce35
jaderyan <jadeelizabethryan@outlook.com>
2020-07-11 15:29:41 +0100

    main container and header styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,28 @@ import Search from "./components/Search";
 import { ArtistStore } from "./stores/store";
 import Results from "./components/Results";
 import { GlobalStyle } from "./styles/globalStyles";
+import styled from "styled-components";
+import { rem } from "polished";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const InnerContainer = styled.div`
+  background-color: white;
+  margin-top: ${rem("48px")};
+  width: 40%;
+  box-shadow: 0px 0px 1px 0px #000000;
+  padding: ${rem("20px")};
+
+  @media (max-width: 640px) {
+    margin-top: ${rem("24px")};
+    width: 80%;
+  }
+`;
 
 export const StoreContext = React.createContext<ArtistStore | null>(null);
 const store = new ArtistStore();
@@ -12,12 +34,14 @@ const store = new ArtistStore();
 function App() {
   return (
     <StoreContext.Provider value={store}>
-      <Fragment>
-        <Header />
-        <Search />
-        <Results />
+      <Container>
+        <InnerContainer>
+          <Header />
+          <Search />
+          <Results />
+        </InnerContainer>
         <GlobalStyle />
-      </Fragment>
+      </Container>
     </StoreContext.Provider>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,26 @@
 import React, { FunctionComponent } from "react";
+import styled from "styled-components";
+import { rem } from "polished";
 
-const Header: FunctionComponent = (props) => (
-  <header>
-    <h1>Search for an artist</h1>
-  </header>
+const Container = styled.header`
+  text-align: center;
+  margin-bottom: ${rem("24px")};
+`;
+
+const HeaderText = styled.h1`
+  font-family: "Poppins", sans-serif;
+  font-weight: bold;
+  font-size: ${rem("40px")};
+
+  @media (max-width: 640px) {
+    font-size: ${rem("32px")};
+  }
+`;
+
+const Header: FunctionComponent = () => (
+  <Container>
+    <HeaderText>Search for an artist</HeaderText>
+  </Container>
 );
 
 export default Header;
