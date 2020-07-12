ac83f28f44bcb9717fbbd491ff769803ddfc4011
jaderyan <jadeelizabethryan@outlook.com>
2020-07-12 15:25:48 +0100

    display songs and open modal with lyrics

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,22 +1,113 @@
-import React, { FunctionComponent, useContext } from "react";
-import { StoreContext } from "../App";
+import React, {
+  FunctionComponent,
+  useContext,
+  useState,
+  Fragment,
+} from "react";
 import { observer } from "mobx-react";
+import Modal from "react-modal";
+import { rem } from "polished";
+import styled from "styled-components";
+import { StoreContext } from "../App";
+import SongButton from "./SongButtton";
+import Loading from "./Loading";
+
+const Container = styled.main`
+  display: flex;
+  flex-wrap: wrap;
+  margin: ${rem("24px")} 0;
+`;
+
+const Lyrics = styled.p`
+  white-space: pre-line;
+`;
+
+const Title = styled.h2`
+  font-family: "Poppins", sans-serif;
+  font-weight: bold;
+  font-size: ${rem("32px")};
+  margin-bottom: ${rem("16px")};
+
+  @media (max-width: 640px) {
+    font-size: ${rem("24px")};
+  }
+`;
+
+const ButtonContainer = styled.header`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: ${rem("16px")};
+`;
+
+const CloseButton = styled.button`
+  border: none;
+  color: #1b1b1b;
+  font-size: ${rem("20px")};
+  background: none;
+`;
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    height: "80%",
+    "overflow-y": "auto",
+  },
+};
 
 const Results: FunctionComponent = () => {
   const store = useContext(StoreContext);
+  const [open, modalOpen] = useState(false);
+  const [title, setTitle] = useState("");
+
   return (
-    <main>
-      {store?.recordings.map((recording) => (
-        <p>
-          <a
-            href={`https://api.lyrics.ovh/v1/${store.artistName}/${recording.title}`}
-            target="_blank"
+    <Container>
+      {store?.loading && <Loading />}
+      {!!store?.recordings.length &&
+        !store.loading &&
+        store?.recordings.map((recording) => (
+          <SongButton
+            song={recording}
+            openModal={() => {
+              modalOpen(!open);
+              setTitle(recording.title);
+            }}
+          />
+        ))}
+
+      <Modal
+        isOpen={open}
+        onRequestClose={() => {
+          modalOpen(!open);
+          setTitle("");
+        }}
+        contentLabel="Example Modal"
+        style={customStyles}
+      >
+        <Fragment>
+          <ButtonContainer
+            onClick={() => {
+              modalOpen(!open);
+              setTitle("");
+            }}
           >
-            {recording.title}
-          </a>
-        </p>
-      ))}
-    </main>
+            <CloseButton>X</CloseButton>
+          </ButtonContainer>
+          <main>
+            <Title>{title}</Title>
+            <Lyrics
+              dangerouslySetInnerHTML={{
+                __html: store?.findLyrics(title) as string,
+              }}
+            ></Lyrics>
+          </main>
+        </Fragment>
+      </Modal>
+    </Container>
   );
 };
 
---
+++ src/components/SongButtton.tsx
@@ -0,0 +1,36 @@
+import React, { FunctionComponent } from "react";
+import { IRecording } from "../types/recording";
+import styled from "styled-components";
+import { rem } from "polished";
+
+interface IProps {
+  song: IRecording;
+  openModal: () => void;
+}
+
+const Button = styled.button`
+  color: #1b1b1b;
+  border: none;
+  padding: ${rem("16px")};
+  margin: ${rem("8px")};
+  font-family: "Open Sans", sans-serif;
+  cursor: pointer;
+
+  &:nth-child(1n + 1) {
+    background-color: #9b98c3;
+  }
+
+  &:nth-child(2n + 1) {
+    background-color: #ed7f6e;
+  }
+
+  &:nth-child(3n + 1) {
+    background-color: #a2c284;
+  }
+`;
+
+const SongButton: FunctionComponent<IProps> = ({ song, openModal }) => (
+  <Button onClick={openModal}>{song.title}</Button>
+);
+
+export default SongButton;
